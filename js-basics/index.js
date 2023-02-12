c8bd8f7a13c894093235fcd4b01bb19e7c2c7400
MacBook <osajielesandra18@gmail.com>
2023-02-12 23:31:58 +0100

    first commit

---
+++ js-basics/index.js
@@ -0,0 +1,22 @@
+// function that multiplies two numbers
+function multiply (number1, number2){
+    return number1 * number2;
+}
+
+console.log(multiply(25, 5));
+
+//Use these variables below to form a sentence and log the sentence in the console.
+//Variables: first name, last name, Stutern track, skills, favourite color, year of your country’s independence.
+
+let firstName = "Esele";
+let lastName = "Osajiele";
+let stuternTrack = "Backend Software Engineer";
+let skills = "HTML, CSS, and JavaScript";
+let favouriteColor = "Black";
+let yearOfCountryIndependence = "1st October, 1960";
+
+console.log("My name is " + firstName + " " + lastName + ", I am a " + stuternTrack + ".")
+
+console.log("I am skilled in " + skills + ".")
+
+console.log("My favorite color is " + favouriteColor + " and my country got her independence on " + yearOfCountryIndependence + ".")
